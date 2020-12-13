27734318f6143d018cbcf78a3f99961bdc00eeed
urbinho88 <urbinho88@users.noreply.github.com>
2020-12-13 13:09:23 +0100

    dodane nowe dzialania do testow

--- test_calculator.py
+++ test_calculator.py
@@ -1,7 +1,24 @@
-mport unittest
+import unittest
+
+
 from calculator import calculate
 
 class TestCalculator(unittest.TestCase):
     def test_dodawanie(self):
         r = calculate(1, 2, '+')
+        self.assertEqual(r, 3)
+
+class TestCalculator(unittest.TestCase):
+    def test_odejmowanie(self):
+        r = calculate(3, 2, '-')
+        self.assertEqual(r, 1)
+
+class TestCalculator(unittest.TestCase):
+    def test_mnozenie(self):
+        r = calculate(1, 2, '*')
+        self.assertEqual(r, 2)
+
+class TestCalculator(unittest.TestCase):
+    def test_dzielenie(self):
+        r = calculate(6, 2, '/')
         self.assertEqual(r, 3)
