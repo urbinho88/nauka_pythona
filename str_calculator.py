30498b68bc76600f4c4c1f93b575b0d603d86331
urbinho88 <urbinho88@users.noreply.github.com>
2020-12-13 14:18:23 +0100

    gotowy

---
+++ str_calculator.py
@@ -0,0 +1,12 @@
+
+def str_calculator(a, b, operacja):
+    if operacja == 'concat':
+        return a + b
+
+def str_calculator(a, b, c, operacja):
+    if operacja == 'concat':
+        return a + b + c
+
+def str_calculator(a, b, c, operacja):
+    if operacja == 'concat':
+        return a + b + c
